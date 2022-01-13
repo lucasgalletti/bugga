142dde9d46b25dfd44c03b88979de9c0ef3f6a95
Lucas <56264817+lucasgalletti@users.noreply.github.com>
2022-01-13 12:43:32 -0300

    Update config.js

--- database/config/config.js
+++ database/config/config.js
@@ -1,16 +1,16 @@
 module.exports = {
   "development": {
-    "username": "z99xyl632hkhazg7",
-    "password": 'ak0fkubhz7bxg34d',
-    "database": "x3le21oduby033ri",
-    "host": "exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "username": "root",
+    "password": null,
+    "database": "bugga",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
-    "username": "z99xyl632hkhazg7",
-    "password": "ak0fkubhz7bxg34d",
-    "database": "x3le21oduby033ri",
-    "host": "exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
