88622d0a74652b0f3fbc91be0bbf53ee7584d25b
Lucas <56264817+lucasgalletti@users.noreply.github.com>
2022-01-13 01:40:11 -0300

    Update config.js

--- database/config/config.js
+++ database/config/config.js
@@ -7,17 +7,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "z99xyl632hkhazg7",
+    "password": "ak0fkubhz7bxg34d",
+    "database": "x3le21oduby033ri",
+    "host": "exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "z99xyl632hkhazg7",
+    "password": "ak0fkubhz7bxg34d",
+    "database": "x3le21oduby033ri",
+    "host": "exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   }
 }
