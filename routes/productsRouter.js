914665452fbca904f2a08acb405b63d90b2a4379
unknown <lucasocanto1996@gmail.com>
2022-01-17 09:17:05 -0300

    validations back products - falta validar fotos

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const uploalBookImg = require('../middleware/upload_booksImg');
 const productController = require('../controllers/productController');
 
+const { body } = require('express-validator');
+
+const validateProductForms = [
+    body('title').trim().notEmpty().withMessage('* Agregá un nombre al producto').bail()
+    .isLength({min: 5}).withMessage('* Agregá un nombre válido').bail(),
+    body('sinopsis').trim().notEmpty().withMessage('* Agregá una sinopsis').bail()
+    .isLength({min: 20}).withMessage('* Debe tener al menos 20 caracteres').bail()
+]
+
 router.get('/admin', productController.admin);
 router.get('/search', productController.search);
 
@@ -10,12 +19,12 @@ router.get('/', productController.index);
 router.get('/carrito', productController.carrito);
 
 router.get('/create', productController.create);
-router.post('/', uploalBookImg.single('bookImg'), productController.store);
+router.post('/', uploalBookImg.single('bookImg'), validateProductForms, productController.store);
 
 router.get('/:id/', productController.detail);
 
 router.get('/edit/:id', productController.edit);
-router.put('/:id', uploalBookImg.single('bookImg'), productController.update);
+router.put('/:id', uploalBookImg.single('bookImg'), validateProductForms,  productController.update);
 
 router.get('/delete/:id', productController.delete);
 router.delete('/delete/:id', productController.destroy);
